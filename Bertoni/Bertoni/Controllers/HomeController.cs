a23e34600af303caf36504d707eea7f78f613e2d
FelixSolano <f.a.s.ch_2103@hotmail.com>
2020-06-27 18:38:35 -0500

    Listo

---
+++ Bertoni/Bertoni/Controllers/HomeController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bertoni.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string DATA = @"{""object"":{""name"":""Name""}}";
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public JsonResult ListAlbunes_old()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://jsonplaceholder.typicode.com/albums");
+            request.Method = "GET";
+            //request.ContentType = "application/json";
+            //request.ContentLength = DATA.Length;
+            return Json(request.ContentLength, "application/json; charset=utf-8", JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ListAlbunes()
+        {
+            //Creamos la URL pasando como parámetro el código de país. Ej: PY, AR, US, ES, MX, etc.
+            var url = string.Format("https://jsonplaceholder.typicode.com/albums");
+
+            //Creamos la solicitud HTTP.
+            var webrequest = (HttpWebRequest)System.Net.WebRequest.Create(url);
+
+            using (var response = webrequest.GetResponse())
+            {
+                //Obtenemos los datos proveídos por el servicio a través del protocolo HTTP.
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    //Obtenemos el resultado retornado por el servicio
+                    var resultado = reader.ReadToEnd();
+                    return Json(resultado, "application/json; charset=utf-8", JsonRequestBehavior.AllowGet);
+                }
+            }
+
+            //Realizamos la llamada al servicio a través del protocolo HTTP.
+            
+        }
+
+
+        public JsonResult listafotos(int idalbum)
+        {
+            //Creamos la URL pasando como parámetro el código de país. Ej: PY, AR, US, ES, MX, etc.
+            var url = string.Format("https://jsonplaceholder.typicode.com/photos?albumId=" + idalbum);
+
+            //Creamos la solicitud HTTP.
+            var webrequest = (HttpWebRequest)System.Net.WebRequest.Create(url);
+
+            using (var response = webrequest.GetResponse())
+            {
+                //Obtenemos los datos proveídos por el servicio a través del protocolo HTTP.
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    //Obtenemos el resultado retornado por el servicio
+                    var resultado = reader.ReadToEnd();
+                    return Json(resultado, "application/json; charset=utf-8", JsonRequestBehavior.AllowGet);
+                }
+            }
+
+            //Realizamos la llamada al servicio a través del protocolo HTTP.
+
+        }
+
+        public JsonResult listacomentarios(int idfoto)
+        {
+            //Creamos la URL pasando como parámetro el código de país. Ej: PY, AR, US, ES, MX, etc.
+            var url = string.Format("https://jsonplaceholder.typicode.com/comments?postId=" + idfoto);
+
+            //Creamos la solicitud HTTP.
+            var webrequest = (HttpWebRequest)System.Net.WebRequest.Create(url);
+
+            using (var response = webrequest.GetResponse())
+            {
+                //Obtenemos los datos proveídos por el servicio a través del protocolo HTTP.
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    //Obtenemos el resultado retornado por el servicio
+                    var resultado = reader.ReadToEnd();
+                    return Json(resultado, "application/json; charset=utf-8", JsonRequestBehavior.AllowGet);
+                }
+            }
+
+            //Realizamos la llamada al servicio a través del protocolo HTTP.
+
+        }
+    }
+}
---
+++ Bertoni/Bertoni/Scripts/JS/Servicios.js
@@ -0,0 +1,76 @@
+﻿$(document).ready(function () {
+
+    fnListar();
+});
+var URL_BASE = window.location;
+function fnListar() {
+    $("#ListaAlbumnes").show();
+    $("#ListaFotos").hide();
+    $.get(URL_BASE + "Home/ListAlbunes", function (response) {
+
+        var tablaDatos = $("#tbdAlbum");
+
+        var arregloDatos = JSON.parse(response);
+         console.log(arregloDatos);
+
+        $.each(arregloDatos, function (index, item) {
+            var filaHTML = "<tr><td>";
+            filaHTML += item.id + "</td>";
+            filaHTML += "<td>" + item["title"] + "</td>";
+            filaHTML += "<td>" + "<a class='btn btn-success' href='javascript:fnListarfotos(\"" + item.id + "\");' >Visualizar Album</a>" + "</td>";
+            filaHTML += "<td>";
+            filaHTML += "</td>";
+            filaHTML += "</tr>";
+            tablaDatos.append(filaHTML);
+        });
+
+
+    });
+}
+
+
+function fnListarfotos(idalbum) {
+    $("#ListaAlbumnes").hide();
+    $("#ListaFotos").show();
+    $.get(URL_BASE + "Home/listafotos?idalbum=" + idalbum, function (response) {
+
+        var tablaDatos = $("#tbdFotos");
+        var arregloDatos = JSON.parse(response);
+        
+        $.each(arregloDatos, function (index, item) {
+            var filaHTML = "<tr><td>";
+            filaHTML += item.id + "</td>";
+            filaHTML += "<td>" + item["title"] + "</td>";
+            filaHTML += "<td>" + "<img src='" + item["url"] + " width='230' height='230''> </img>"+  "</td>";
+            filaHTML += "<td>" + "<a class='btn btn-success' href='javascript:fnListarComentarios(\"" + item.id + "\");'>Ver comentarios</a>" + "</td>";
+            filaHTML += "</tr>";
+            tablaDatos.append(filaHTML);
+        });
+
+
+    });
+}
+
+function fnListarComentarios(idfoto) {
+    $("#ListaAlbumnes").hide();
+    $("#ListaFotos").show();
+    $.get(URL_BASE + "Home/listacomentarios?idfoto=" + idfoto, function (response) {
+        $("#tbdComentarios").html("");
+        var tablaDatos = $("#tbdComentarios");
+        var arregloDatos = JSON.parse(response);
+
+        $.each(arregloDatos, function (index, item) {
+            cont = index + 1;
+            var filaHTML = "<tr><td>";
+            filaHTML += cont + "</td>";
+            filaHTML += "<td>" + item["name"] + "</td>";
+            filaHTML += "<td>" + item["email"]  + "</td>";
+            filaHTML += "<td>" + item["body"] + "</td>";
+            filaHTML += "</tr>";
+            tablaDatos.append(filaHTML);
+        });
+    });
+}
+
+
+
